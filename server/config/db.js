7f8b431416efd9e153fd650afc8efa34b0a0dcb4
Dilmi Kottachchi <dilmi2790@gmail.com>
2024-07-29 18:47:41 +0530

    Mongo URI update

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        // const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect("mongodb+srv://admin:tlCC7CbygwKwdaxL@migrate-guide.c2cqltx.mongodb.net/?retryWrites=true&w=majority&appName=migrate-guide");
+
         console.log(`MongoDB connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error ${error.message}`);
