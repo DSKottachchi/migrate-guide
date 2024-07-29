ff111d49eb2886f46aa2137808541f417bb2386b
Dilmi Kottachchi <dilmi2790@gmail.com>
2024-07-29 18:28:21 +0530

    Update export

--- ecosystem.config.mjs
+++ ecosystem.config.mjs
@@ -1,27 +1,28 @@
-module.exports = {
-    apps: [
-      {
-        name: 'client',
-        script: './client/server.js',
-        env: {
-          NODE_ENV: 'development',
-          // You can add more environment-specific variables here if needed
-        },
-        env_production: {
-          NODE_ENV: 'production',
-          // You can add more environment-specific variables here if needed
-        },
+const ecosystem = (req, res, next) => {
+  apps: [
+    {
+      name: "client",
+      script: "./client/server.js",
+      env: {
+        NODE_ENV: "development",
+        // You can add more environment-specific variables here if needed
       },
-      {
-        name: 'server',
-        script: './server/server.js',
-        env: {
-          NODE_ENV: 'development',
-        },
-        env_production: {
-          NODE_ENV: 'production',
-        },
+      env_production: {
+        NODE_ENV: "production",
+        // You can add more environment-specific variables here if needed
       },
-    ],
-  };
-  
+    },
+    {
+      name: "server",
+      script: "./server/server.js",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ];
+};
+
+export { ecosystem };
